136723e832d10fe50bbd1be6dd8d55872903dc53
Tasneem Koushar <tasneemkoushar@pop-os.localdomain>
2022-01-19 14:03:58 +0530

    Day4

--- Day16-20_Adobe/quest9.cpp
+++ Day16-20_Adobe/quest9.cpp
@@ -6,20 +6,33 @@
 class Solution{
   public:
     string nextPalin(string N) { 
-        int len = N.size();
+        int len = N.size()/2;
        
-            int last = len/2;
-            bool val = next_permutation(N.begin(),N.begin()+last);
-           
-            if(val==false)
+        for(int i=len-1; i>0; i--)
+        {
+            if(N[i]>N[i-1])
             {
-                return "-1";
+                int index = len-1;
+                while(index>=i)
+                {
+                    if(N[index]>N[i-1])
+                    {
+                        swap(N[i-1],N[index]);
+                        break;
+                    }
+                    index--;
+                }
+                if(index!=i-1)
+                {
+                    reverse(N.begin()+i,N.begin()+len);
+                }
+                for(int i=0; i<len; i++)
+                {
+                    N[N.size()-1-i] = N[i];
+                }
+                return N;
             }
-            for(int i=0; i<last; i++)
-            {
-                N[N.size()-1-i] = N[i];
-            }
-        
-        return N;
+        }
+        return "-1";
     }
 };
